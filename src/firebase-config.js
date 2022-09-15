a1e380a9845a1fae62e9f1bdeaf395ab2bb4e6a1
Jay Romel <jayromel.lawas@yens.ph>
2022-09-15 22:07:48 +0800

    New Update

---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeuYrli59uQTKinBzfSkgXvksmJ9uqgvs",
+    authDomain: "fir-tutorial-81fa5.firebaseapp.com",
+    projectId: "fir-tutorial-81fa5",
+    storageBucket: "fir-tutorial-81fa5.appspot.com",
+    messagingSenderId: "503726364914",
+    appId: "1:503726364914:web:03639c88e0c1850f44986b",
+    measurementId: "G-HS8QVS7ECK"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+ export const db = getFirestore();
+
+
+
+
+
